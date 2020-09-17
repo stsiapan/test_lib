9067fdcb8bae8ac889d477463869b9a53d72a8cb
stsiapan <55788184+stsiapan@users.noreply.github.com>
2020-09-17 16:33:00 +0300

    Update aws.groovy

--- vars/aws.groovy
+++ vars/aws.groovy
@@ -13,9 +13,10 @@ def copyToBucket(envir, version, s3bucket) {
 def copyFromBucket(envir, version, s3bucket) {
     try {
         def copyFrom = sh(returnStdout: true,
-                         script: sh "aws s3 cp s3://${s3bucket}/${envir}/${version}/ ./ --recursive"
-                         ).trim()
-        println("CORRECT log message") 
+                        script: sh "aws s3 cp s3://${s3bucket}/${envir}/${version}/ ./ --recursive" 
+                        ).trim()
+        println(copyFrom)
+        println("CORRECT log message")
     } catch (err) {
         println("ERROR log message")
         print(err)
